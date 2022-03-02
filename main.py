ae598ec8eb1598db803f38c1a4443c527dfa7313
River Kelly <65210753+RK311y@users.noreply.github.com>
2022-03-02 08:40:06 -0700

    Setup main.py

--- main.py
+++ main.py
@@ -4,4 +4,10 @@ CSCI-455: Embedded Systems (Robotics)
 Spring 2022
 '''
 
+from src import KeyBindings, TangBotController
+
+if __name__ == "__main__":
+    bot_controller = TangBotController()
+    key_bindings = KeyBindings(bot_controller)
+
 # END main.py
