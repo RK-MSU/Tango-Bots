f40b7129fbf2bd102fedf548a7788efe12b1fc3e
River Kelly <65210753+RK311y@users.noreply.github.com>
2022-03-04 11:40:38 -0700

    Test Wheel Speed Forwards

--- src/tango_bot.py
+++ src/tango_bot.py
@@ -39,8 +39,8 @@ class TangBotController:
     # constructor
     def __init__(self):
         self.usb = getUSB()
-        # if self.usb is not None:
-        #     self.usb.write_timeout = 1  # TEST THIS
+        if self.usb is not None:
+            self.usb.write_timeout = 0.1  # TEST THIS
         self.running = True
         self.lock = threading.Lock()
         # Exit Safe Start
