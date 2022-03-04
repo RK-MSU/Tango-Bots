629e4db524bea440deaf888fa365f0e6e3b069b1
Kyler Gappa <kyler.gappa@student.montana.edu>
2022-03-03 18:02:37 -0700

    Speed changes

--- src/key_bindings.py
+++ src/key_bindings.py
@@ -93,13 +93,13 @@ class KeyBindings:
         print("Keycode: ", keycode)
         if keycode == 44:
             log.debug('Key Pressed: "%s"', '<j>')
-            self.bot.SPEED = 800
+            self.bot.SPEED = 100
         elif keycode == 45:
             log.debug('Key Pressed: "%s"', '<k>')
             self.bot.SPEED = 500
         elif keycode == 46:
             log.debug('Key Pressed: "%s"', '<l>')
-            self.bot.SPEED = 200
+            self.bot.SPEED = 800
         elif keycode == 58:
             log.debug('Key Pressed: "%s"', '<m>')
             self.bot.stopMoving()
