df488850f3ec421ca8785e4922b998ec31bdff0b
River Kelly <65210753+RK311y@users.noreply.github.com>
2022-04-05 07:53:29 -0600

    Update dialog to read lines from file

--- src/dialog.py
+++ src/dialog.py
@@ -1,8 +1,24 @@
 # dialog.py
 
+import os
+from .log import log
+
 class Dialog:
 
-    def __init__(self):
-        print("Dialog")
+    lines: list = list()
+
+    def __init__(self, file: str = 'dialog.txt'):
+        self.readFile(file)
+        print(self.lines)
+
+    def readFile(self, file: str):
+        if os.path.exists(file):
+            with open(file, 'r') as file:
+                self.lines = file.readlines()
+            # clean lines by stripping whitespace
+            for index, line in enumerate(self.lines):
+                self.lines[index] = str(line).strip()
+        else:
+            log.critical('Unable to open "%s"', file)
 
 # END
